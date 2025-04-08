92bfdd1c68d402b3b336f208b575d478accf3cef
Rathin Bagchi <rathinb745@gmail.com>
2025-04-08 15:40:46 +0530

    first commit

---
+++ models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/mini-project");
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose is connected");
+});
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  name: String,
+  age: Number,
+  email: String,
+  password: String,
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }],
+});
+
+module.exports = mongoose.model("user", userSchema);
